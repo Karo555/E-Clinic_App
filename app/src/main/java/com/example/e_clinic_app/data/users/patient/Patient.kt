21f8e6edd1e2d48ba24fdd3cf44f30f3bfb97901
Karolina Nowacka <278427@student.pwr.edu.pl>
2025-05-27 11:44:00 +0200

    add documentation to files in /data/users/patient

--- app/src/main/java/com/example/e_clinic_app/data/users/patient/Patient.kt
+++ app/src/main/java/com/example/e_clinic_app/data/users/patient/Patient.kt
@@ -4,7 +4,20 @@ import com.example.e_clinic_app.data.users.Role
 import com.example.e_clinic_app.data.users.UsersEntity
 import java.util.Date
 import java.util.UUID
-
+/**
+ * Represents a patient user in the e-clinic application.
+ *
+ * This data class extends the `UsersEntity` class and provides additional properties
+ * specific to patient users, such as their date of birth.
+ *
+ * @property id The unique identifier for the patient user.
+ * @property firstName The first name of the patient user.
+ * @property lastName The last name of the patient user.
+ * @property email The email address of the patient user.
+ * @property role The role of the user, which is set to `Role.PATIENT` by default.
+ * @property phoneNumber The optional phone number of the patient user.
+ * @property dateOfBirth The date of birth of the patient.
+ */
 data class Patient(
     override val id: UUID,
     override val firstName: String,
