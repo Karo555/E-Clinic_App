34cf72d26bbdd44e21df75f5cdb602540fa53371
Karolina Nowacka <278427@student.pwr.edu.pl>
2025-05-27 11:40:54 +0200

    add documentation to files in /data/users/admin

--- app/src/main/java/com/example/e_clinic_app/data/users/admin/Admin.kt
+++ app/src/main/java/com/example/e_clinic_app/data/users/admin/Admin.kt
@@ -4,6 +4,20 @@ import com.example.e_clinic_app.data.users.Role
 import com.example.e_clinic_app.data.users.UsersEntity
 import java.util.UUID
 
+/**
+ * Represents an admin user in the e-clinic application.
+ *
+ * This data class extends the `UsersEntity` class and provides additional properties
+ * specific to admin users, such as the associated clinic ID.
+ *
+ * @property id The unique identifier for the admin user.
+ * @property firstName The first name of the admin user.
+ * @property lastName The last name of the admin user.
+ * @property email The email address of the admin user.
+ * @property role The role of the user, which is set to `Role.ADMIN` by default.
+ * @property phoneNumber The optional phone number of the admin user.
+ * @property clinicsId The unique identifier of the clinic associated with the admin user.
+ */
 data class Admin(
     override val id: UUID,
     override val firstName: String,
